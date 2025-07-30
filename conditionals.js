29d5bc5cf3743b63620beed615093cb794c50d60
Mr-Ossom <ossomdavid414@gmail.com>
2025-07-30 17:30:09 +0100

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,7 @@
+function sumThreeNumbers(number1, number2, number3){
+    const sum = number1 + number2 + number3;
+
+    return sum;
+}
+
+console.log(sumThreeNumbers(10, 10, 30));
---
+++ myscript.js
@@ -0,0 +1,41 @@
+let x=7
+let y=3
+let ans=(x+y)
+console.log(ans);
+
+let j=30000
+let l=100
+let answ=(j%l)
+console.log(answ);
+
+let m=30
+let n=2
+let answer=(m / n)
+console.log(answer);
+
+let a1=72
+let a2="72"
+console.log(a1===a2)
+
+let b10=false
+let b2=true
+console.log(b10||b2)
+
+let c1=10
+let c2=13
+console.log(c1>c2)
+console.log(c1<c2)
+console.log(c1<=c2)
+
+let d1=30
+let d2="30"
+console.log(d1==d2)
+console.log(d1===d2)
+console.log(d1!==d2)
+console.log(d1!=d2)
+
+// Arrays
+
+let students = ["David", "Lamech", "Richard", "Doe"]
+console.log(students[3])
+console.log(students[1])
